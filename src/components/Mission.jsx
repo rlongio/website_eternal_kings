7a8abbcc051500ea006db9d0706d01b25c7a2ebf
Ryan Long <ryan@rlong.io>
2020-10-26 15:42:01 -0400

    Updated mission statement

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -9,19 +9,16 @@ class Mission extends React.Component {
             <div className='jumbotron'>
               <h1 className='display-4'>Mission Statement</h1>
               <p className='lead'>
-                Eternal Kings Motorcycle Society is a men's only patchless
-                motorcycle riding club/brotherhood. We do not wear back patches
-                as we want to remain 100% neutral with everyone.
+                Eternal Kings Motorcycle Society is a brotherhood of men that share a common interest in motorcycle culture and riding motorcycles.
                 <br />
                 <br />
-                We are a family focused brotherhood. We ride together, travel
-                together, party together and help each other throughout life.
+                We are not a traditional motorcycle club. We do not wear back patches as we want to remain 100% neutral with everyone.
                 <br />
                 <br />
-                Our motto is "Leave Your Mark" as we believe we can make a
-                difference as we ride through life. Our logo is "The Eternal
-                King" symbolizing that with a unified brotherhood of men we can
-                all be Kings who leave a legacy.
+                We are family focused. We ride together, travel together, party together and help each other throughout life.
+                <br />
+                <br />
+                Our motto is "Leave Your Mark" as we believe we can make a difference as we ride through life. Our logo is "The Eternal King" symbolizing that with a unified brotherhood of men we can all be Kings who leave a legacy.
                 <br />
                 <br />
                 We look forward to riding with you. Remember to always "Leave
